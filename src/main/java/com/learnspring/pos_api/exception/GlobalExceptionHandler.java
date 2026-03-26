d7a1f79aeff370943b580c59fa223e68ce314a7a
raffinauvall <raffinauvaltaqy@gmail.com>
2026-03-26 14:21:36 +0700

    feat: add exception handler

--- src/main/java/com/learnspring/pos_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/learnspring/pos_api/exception/GlobalExceptionHandler.java
@@ -1,11 +1,36 @@
 package com.learnspring.pos_api.exception;
 
 import com.learnspring.pos_api.dto.ApiResponse;
+import org.hibernate.annotations.NotFound;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
-    public ApiResponse<Void> handdleRuntime(RuntimeException e) {
-        return new ApiResponse<>("Error", e.getMessage(), null);
+    public ResponseEntity<ApiResponse<Object>> handleRuntime(RuntimeException ex) {
+        return new ResponseEntity<>(
+                new ApiResponse<>("error", ex.getMessage(), null),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleGeneral(Exception ex) {
+        return new ResponseEntity<>(
+                new ApiResponse<>("error", "Something when wrong", null),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleNotFound(NotFoundException ex) {
+        return new ResponseEntity<>(
+                new ApiResponse<>("error", ex.getMessage(), null),
+                HttpStatus.NOT_FOUND
+        );
     }
 }
---
+++ src/main/java/com/learnspring/pos_api/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.learnspring.pos_api.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
