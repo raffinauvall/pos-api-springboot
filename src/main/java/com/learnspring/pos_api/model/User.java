bdfa88cfe8cf898466bd6b9993afe0cb26c89ed8
raffinauvall <raffinauvaltaqy@gmail.com>
2026-03-27 08:41:28 +0700

    feat: add entity user

---
+++ src/main/java/com/learnspring/pos_api/model/User.java
@@ -0,0 +1,17 @@
+package com.learnspring.pos_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+}
